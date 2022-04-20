70b71abee419766ee9ec7dc0e916f0580fe3d6a6
stbnvlrd <estebanvelardegarcez@gmail.com>
2022-04-20 10:59:33 -0500

    Changing choicing form from text to buttons

--- javascript.js
+++ javascript.js
@@ -1,6 +1,15 @@
-const guessSubmit = document.querySelector('.guessSubmit');
+const guessRock = document.querySelector('.guessRock');
+const guessPaper = document.querySelector('.guessPaper');
+const guessScissor = document.querySelector('.guessScissor');
 const guessField = document.querySelector('.guessField');
+const choices = document.querySelector('.choices');
+const lastRound = document.querySelector('.lastRound');
+const winResult = document.querySelector('.winResult');
+const lostResult = document.querySelector('.lostResult');
 var playerSelect = '';
+var winning = 0;
+var losses = 0;
+var sum = 0;
 // var computerSelection = '';
 
 function computerPlay(){
@@ -19,45 +28,73 @@ function computerPlay(){
   return computerSelect
 }
 
-function playerChoice(userSelect){
-  if(userSelect.toLowerCase() === 'rock') {
-    playerSelect = 'Rock';
-  } else if(userSelect.toLowerCase() === 'paper') {
-    playerSelect = 'Paper';
-  } else if(userSelect.toLowerCase() === 'scissor') {
-    playerSelect = 'Scissor';
-  } else {
-    window.alert("Player selected unknown option, Rock is choose by defalut");
-    playerSelect = 'Rock';
-  }
-  console.log(playerSelect);
-}
-
-function checkGuess() {
-
-  // const userSelect = guessField.value;
-  const userSelect = guessField.value;
-  computerSelection = computerPlay();
-  playerChoice(userSelect);
-  playRound(playerSelect, computerSelection);
-}
+// function playerChoice(userSelect){
+//   if(userSelect.toLowerCase() === 'rock') {
+//     playerSelect = 'Rock';
+//   } else if(userSelect.toLowerCase() === 'paper') {
+//     playerSelect = 'Paper';
+//   } else if(userSelect.toLowerCase() === 'scissor') {
+//     playerSelect = 'Scissor';
+//   } else {
+//     window.alert("Player selected unknown option, Rock is choose by defalut");
+//     playerSelect = 'Rock';
+//   }
+//   console.log(playerSelect);
+// }
 
 function playRound(playerSelection, computerSelection) {
   console.log(playerSelection);
   console.log(computerSelection);
-  var messageText = "Computer select " + computerSelection + " and you selected " + playerSelection + "\n";
-  if(playerSelect === computerSelection){
-    window.alert(messageText + "Its a Tie");
-  } else if ((playerSelect === 'Paper' && computerSelection === 'Rock') || (playerSelect === 'Rock' && computerSelection === 'Scissor') || (playerSelect === 'Scissor' && computerSelection === 'Paper')) {
-    window.alert(messageText + "You Win!");
-  } else if ((playerSelect === 'Rock' && computerSelection === 'Paper') || (playerSelect === 'Paper' && computerSelection === 'Scissor') || (playerSelect === 'Scissor' && computerSelection === 'Rock')) {
-    window.alert(messageText + "You Lose!");
+  sum = sum + 1;
+  var messageText = "Computer select " + computerSelection + " and you selected " + playerSelection;
+  choices.textContent = messageText;
+  if(playerSelection === computerSelection){
+    messageText = "Its a Tie!";
+  } else if ((playerSelection === 'Paper' && computerSelection === 'Rock') || (playerSelection === 'Rock' && computerSelection === 'Scissor') || (playerSelection === 'Scissor' && computerSelection === 'Paper')) {
+    messageText = "You Win this round!";
+    winning = winning + 1;
+  } else if ((playerSelection === 'Rock' && computerSelection === 'Paper') || (playerSelection === 'Paper' && computerSelection === 'Scissor') || (playerSelection === 'Scissor' && computerSelection === 'Rock')) {
+    messageText = "You Lose this round!";
+    losses = losses + 1;
   }
+
+  lastRound.textContent = messageText;
+  messageText = "Winning: " + winning;
+  winResult.textContent = messageText;
+  messageText = "Lossing: " + losses;
+  lostResult.textContent = messageText;
 }
 
+function checkRock() {
 
+  const userSelect = 'Rock';
+  console.log(userSelect);
+  computerSelection = computerPlay();
+  // playerChoice(userSelect);
+  playRound(userSelect, computerSelection);
+}
 
+function checkPaper() {
 
-guessSubmit.addEventListener('click', checkGuess);
+  const userSelect = 'Paper';
+  console.log(userSelect);
+  computerSelection = computerPlay();
+  // playerChoice(userSelect);
+  playRound(userSelect, computerSelection);
+}
+
+function checkScissor() {
+
+  const userSelect = 'Scissor';
+  console.log(userSelect);
+  computerSelection = computerPlay();
+  // playerChoice(userSelect);
+  playRound(userSelect, computerSelection);
+}
+
+guessRock.addEventListener('click', checkRock);
+guessPaper.addEventListener('click', checkPaper);
+guessScissor.addEventListener('click', checkScissor);
+
 
 
